adf2940f140056c3e9f0a83ea43327af37061cd7
Амир Абдрафиков <sequze@mail.ru>
2025-07-21 15:00:52 +0300

    sqla repository

---
+++ app/repositories/base_repository.py
@@ -0,0 +1,30 @@
+from sqlalchemy.ext.asyncio.session import AsyncSession
+from sqlalchemy import insert, select, delete
+
+
+class SQlAlchemyRepository:
+    model = None
+
+    async def create(self, session: AsyncSession, data: dict) -> int:
+        stmt = insert(self.model).values(**data).returning(self.model)
+        result = await session.execute(stmt)
+        await session.commit()
+        return result.scalar()
+
+    async def get_all(self, session: AsyncSession):
+        res = await session.scalars(select(self.model))
+        return [user for user in res]
+
+    async def get_by_filters(self, session: AsyncSession, filters: dict, one: bool = True):
+        stmt = select(self.model).filter_by(**filters)
+        res = await session.execute(stmt)
+        if one:
+            return res.one_or_none()
+        return res.scalars().all()
+
+    async def delete_by_id(self, session: AsyncSession, entity_id: int):
+        res = await session.execute(
+            delete(self.model)
+            .where(self.model.id == entity_id).returning(self.model))
+        await session.commit()
+        return res.scalar()
