43ad13b17a7258b8fa3bcc8cf1c26e00a53985fd
Амир Абдрафиков <sequze@mail.ru>
2025-07-21 15:08:39 +0300

    import user router

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,3 +1,10 @@
 from fastapi import APIRouter
-
+from .users import router as users_router
+from core.config import settings
 router = APIRouter()
+
+router.include_router(
+    users_router,
+    tags=["Users"],
+    prefix=settings.api_prefix.users,
+)
