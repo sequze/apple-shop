0c8851bb6cc2d4d5c36043e1469bb2ecf2919a74
Амир Абдрафиков <sequze@mail.ru>
2025-07-22 14:49:49 +0300

    add category router to api

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,10 +1,18 @@
 from fastapi import APIRouter
 from .users import router as users_router
+from .categories import router as categories_router
 from core.config import settings
+
 router = APIRouter()
 
 router.include_router(
     users_router,
     tags=["Users"],
     prefix=settings.api_prefix.users,
+)
+
+router.include_router(
+    categories_router,
+    tags=["Categories"],
+    prefix=settings.api_prefix.categories
 )
