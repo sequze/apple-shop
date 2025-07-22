f6bdef5b4deeeed6206bf6f78ee36de3286e828f
Амир Абдрафиков <sequze@mail.ru>
2025-07-22 14:49:26 +0300

    category dependency

--- app/api/dependencies.py
+++ app/api/dependencies.py
@@ -1,8 +1,18 @@
 from core.models import db_helper
+from services.category.repository import CategoryRepository
+from services.category.service import CategoryService
 from services.user.service import UserService
 from repositories.uow import UnitOfWork
 from services.user.repository import UserRepository
 
 
+def unit_of_work() -> UnitOfWork:
+    return UnitOfWork(db_helper.session_factory)
+
+
 def users_service() -> UserService:
-    return UserService(UserRepository(), UnitOfWork(db_helper.session_factory))
+    return UserService(UserRepository(), unit_of_work())
+
+
+def category_service() -> CategoryService:
+    return CategoryService(CategoryRepository(), unit_of_work())
