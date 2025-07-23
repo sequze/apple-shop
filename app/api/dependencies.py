1f8df6c9dae23254fdeed79b9a4ec8bb55fe3f92
Амир Абдрафиков <sequze@mail.ru>
2025-07-23 11:41:55 +0300

    feat: add product service dependency

--- app/api/dependencies.py
+++ app/api/dependencies.py
@@ -1,6 +1,8 @@
 from core.models import db_helper
 from services.category.repository import CategoryRepository
 from services.category.service import CategoryService
+from services.product.repository import ProductRepository
+from services.product.service import ProductService
 from services.user.service import UserService
 from repositories.uow import UnitOfWork
 from services.user.repository import UserRepository
@@ -16,3 +18,7 @@ def users_service() -> UserService:
 
 def category_service() -> CategoryService:
     return CategoryService(CategoryRepository(), unit_of_work())
+
+
+def product_service() -> ProductService:
+    return ProductService(ProductRepository(), unit_of_work())
