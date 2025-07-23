99722109edf043667f22b8767266fdffca1d7ba0
Амир Абдрафиков <sequze@mail.ru>
2025-07-23 18:52:51 +0300

    feat: order schemas

---
+++ app/services/order/schemas.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+
+from pydantic import BaseModel, NonNegativeFloat, PositiveInt
+
+from core.models.order import OrderStatus
+from services.order_item.schemas import OrderItemDTO
+
+
+class OrderBaseSchema(BaseModel):
+    status: OrderStatus
+    user_id: PositiveInt
+
+
+class OrderDTO(OrderBaseSchema):
+    id: PositiveInt
+    created_at: datetime
+    total_amount: NonNegativeFloat
+
+    class Config:
+        from_attributes = True
+
+
+class OrderDetailedDTO(OrderDTO):
+    items: list[OrderItemDTO]
+
+
+class OrderCreateSchema(OrderBaseSchema):
+    pass
+
+
+class OrderUpdateSchema(OrderBaseSchema):
+    status: OrderStatus | None = None
+    user_id: PositiveInt | None = None
