bc1044c6d229dc716b23d026278699e46d7be761
Амир Абдрафиков <sequze@mail.ru>
2025-07-23 18:55:46 +0300

    feat: order views

---
+++ app/api/orders/views.py
@@ -0,0 +1,69 @@
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, HTTPException, status
+
+from api.dependencies import order_service
+from services.order.schemas import OrderDTO, OrderCreateSchema, OrderUpdateSchema
+from services.order.service import OrderService, OrderNotFoundError
+
+router = APIRouter()
+
+order_service_dep = Annotated[OrderService, Depends(order_service)]
+
+
+@router.get("/")
+async def get_all_orders(
+        order_service: order_service_dep,
+) -> list[OrderDTO]:
+    return await order_service.get_all()
+
+
+@router.get("/{order_id}")
+async def get_order(
+        order_id: int,
+        order_service: order_service_dep,
+) -> OrderDTO:
+    try:
+        return await order_service.get_by_id(order_id)
+    except OrderNotFoundError:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Order not found",
+        )
+
+
+@router.post("/")
+async def create_order(
+        order_service: order_service_dep,
+        data: OrderCreateSchema,
+) -> OrderDTO:
+    return await order_service.create(data)
+
+
+@router.patch("/{order_id}")
+async def update_order(
+        order_id: int,
+        data: OrderUpdateSchema,
+        order_service: order_service_dep,
+) -> OrderDTO:
+    try:
+        return await order_service.update(data, order_id)
+    except OrderNotFoundError:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Order not found"
+        )
+
+
+@router.delete("/{order_id}")
+async def delete_order(
+        order_id: int,
+        order_service: order_service_dep,
+) -> OrderDTO:
+    try:
+        return await order_service.delete(order_id)
+    except OrderNotFoundError:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Order not found"
+        )
