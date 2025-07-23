c30a0402256c465713720ef093b1579dfd99de3a
Амир Абдрафиков <sequze@mail.ru>
2025-07-23 18:53:21 +0300

    feat: order service

---
+++ app/services/order/service.py
@@ -0,0 +1,54 @@
+from sqlalchemy.ext.asyncio.session import AsyncSession
+
+from core.models import Order
+from repositories.uow import UnitOfWork
+from services.order.repository import OrderRepository
+from services.order.schemas import OrderDTO, OrderCreateSchema, OrderUpdateSchema, OrderDetailedDTO
+
+
+class OrderNotFoundError(Exception):
+    pass
+
+
+class OrderService:
+    # create update delete get by id get all
+    def __init__(
+            self,
+            repository: OrderRepository,
+            uow: UnitOfWork,
+    ):
+        self.repository = repository
+        self.uow = uow
+
+    async def __find_by_id(self, session: AsyncSession, id: int) -> Order:
+        order = await self.repository.get_by_id(session, id)
+        if order is None:
+            raise OrderNotFoundError
+        return order
+
+    async def get_by_id(self, id: int) -> OrderDetailedDTO:
+        async with self.uow as uow:
+            order = await self.repository.get_with_items(uow.session, {"id": id}, True)
+            return OrderDetailedDTO.model_validate(order)
+
+    async def create(self, data: OrderCreateSchema) -> OrderDTO:
+        async with self.uow as uow:
+            order = await self.repository.create(uow.session, data.model_dump())
+            return OrderDTO.model_validate(order)
+
+    async def update(self, data: OrderUpdateSchema, id: int) -> OrderDTO:
+        async with self.uow as uow:
+            order = await self.__find_by_id(uow.session, id)
+            order_updated = await self.repository.update(uow.session, data.model_dump(exclude_unset=True), order)
+            return OrderDTO.model_validate(order_updated)
+
+    async def delete(self, id: int) -> OrderDTO:
+        async with self.uow as uow:
+            order = await self.__find_by_id(uow.session, id)
+            await self.repository.delete(uow.session, order)
+            return OrderDTO.model_validate(order)
+
+    async def get_all(self) -> list[OrderDTO]:
+        async with self.uow as uow:
+            orders = await self.repository.get_all(uow.session)
+            return [OrderDTO.model_validate(order) for order in orders]
