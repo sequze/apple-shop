1e6a46695df7c5c9acb08d0a7907df7730fb786b
Амир Абдрафиков <sequze@mail.ru>
2025-07-23 13:11:13 +0300

    feat: connect discount router to main router

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -3,6 +3,7 @@ from .users import router as users_router
 from .categories import router as categories_router
 from .products import router as products_router
 from core.config import settings
+from .discounts import router as discounts_router
 
 router = APIRouter()
 
@@ -22,4 +23,10 @@ router.include_router(
     products_router,
     tags=["Products"],
     prefix=settings.api_prefix.products
+)
+
+router.include_router(
+    discounts_router,
+    tags=["Discounts"],
+    prefix=settings.api_prefix.discounts
 )
