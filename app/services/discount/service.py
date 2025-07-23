79e3a005572228a3019447e8da7de23fbe3b841a
Амир Абдрафиков <sequze@mail.ru>
2025-07-23 13:10:37 +0300

    feat: add discount service

---
+++ app/services/discount/service.py
@@ -0,0 +1,75 @@
+from sqlalchemy.ext.asyncio.session import AsyncSession
+
+from core.models import Discount
+from repositories.uow import UnitOfWork
+from .repository import DiscountRepository
+from .schemas import DiscountCreateSchema, DiscountDTO, DiscountUpdateSchema
+
+
+class DiscountNotFoundError(Exception):
+    pass
+
+
+class InvalidTimeIntervalError(Exception):
+    """End date bigger then start date"""
+    pass
+
+
+class DiscountService:
+
+    def __init__(
+            self,
+            repository: DiscountRepository,
+            uow: UnitOfWork,
+    ):
+        self.repository = repository
+        self.uow = uow
+
+    async def __find_by_id(self, session: AsyncSession, id: int) -> Discount:
+        discount = await self.repository.get_by_id(session, id)
+        if discount is None:
+            raise DiscountNotFoundError
+        return discount
+
+    async def create(
+            self,
+            data: DiscountCreateSchema
+    ) -> DiscountDTO:
+        async with self.uow as uow:
+            res = await self.repository.create(uow.session, data.model_dump())
+            return DiscountDTO.model_validate(res)
+
+    async def get_by_id(self, id: int) -> DiscountDTO:
+        async with self.uow as uow:
+            discount = await self.__find_by_id(uow.session, id)
+            return DiscountDTO.model_validate(discount)
+
+    async def get_all(self) -> list[DiscountDTO]:
+        async with self.uow as uow:
+            res = await self.repository.get_all(uow.session)
+            return [DiscountDTO.model_validate(discount) for discount in res]
+
+    async def delete(self, id: int) -> DiscountDTO:
+        async with self.uow as uow:
+            discount = await self.__find_by_id(uow.session, id)
+            await self.repository.delete(uow.session, discount)
+            return DiscountDTO.model_validate(discount)
+
+    async def update(self, id: int, data: DiscountUpdateSchema) -> DiscountDTO:
+        async with self.uow as uow:
+            discount = await self.__find_by_id(uow.session, id)
+            if data.end_date or data.start_date:
+                end_date = data.end_date.replace(tzinfo=None) if data.end_date else discount.end_date
+                start_date = data.start_date.replace(tzinfo=None) if data.start_date else discount.start_date
+                if end_date <= start_date:
+                    raise InvalidTimeIntervalError
+            discount_updated = await self.repository.update(
+                uow.session,
+                data.model_dump(exclude_unset=True),
+                discount)
+            return DiscountDTO.model_validate(discount_updated)
+
+    async def deactivate(self, id: int) -> None:
+        async with self.uow as uow:
+            discount = await self.__find_by_id(uow.session, id)
+            await self.repository.deactivate(uow.session, discount)
