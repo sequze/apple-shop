6f31c715f03ab0e3a23a0323c5d73e74969598a1
Амир Абдрафиков <sequze@mail.ru>
2025-07-24 14:17:20 +0300

    feat: added uq for user_id and product_id in CartItem

--- app/core/models/cart_item.py
+++ app/core/models/cart_item.py
@@ -2,9 +2,10 @@ from typing import TYPE_CHECKING
 
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import mapped_column, Mapped, relationship
-from datetime import datetime, timezone
+from sqlalchemy.sql.schema import UniqueConstraint
+
 from .base import Base
-from .mixins import IntIdPkMixin, CreatedAtMixin
+from .mixins import IntIdPkMixin
 
 if TYPE_CHECKING:
     from .user import User
@@ -17,3 +18,7 @@ class CartItem(IntIdPkMixin, Base):
     product_id: Mapped[int] = mapped_column(ForeignKey("products.id"))
     user: Mapped["User"] = relationship(back_populates="cart_items")
     product: Mapped["Product"] = relationship(lazy="selectin")
+
+    __table_args__ = (
+        UniqueConstraint("user_id", "product_id"),
+    )
