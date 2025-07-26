a4a3c375b0d7c4b56d79ea5419f5b8291eaca2ce
Амир Абдрафиков <sequze@mail.ru>
2025-07-26 15:56:59 +0300

    feat: add functions to encode and decode jwt token, hash and verify password

---
+++ app/core/auth/utils.py
@@ -0,0 +1,37 @@
+import jwt
+from core.config import settings
+import bcrypt
+from passlib.context import CryptContext
+
+
+def encode_jwt(
+        payload: dict,
+        private_key: str = settings.auth_jwt.private_key_path.read_text(),
+        algorithm: str = settings.auth_jwt.algorithm,
+) -> str:
+    encoded_jwt = jwt.encode(payload, private_key, algorithm=algorithm)
+    return encoded_jwt
+
+
+def decode_jwt(
+        jwt_token: str | bytes,
+        public_key: str = settings.auth_jwt.public_key_path.read_text(),
+        algorithm: str = settings.auth_jwt.algorithm,
+) -> str:
+    decoded_jwt = jwt.decode(
+        jwt_token,
+        public_key,
+        algorithms=[algorithm],
+    )
+    return decoded_jwt
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password: str) -> str:
+    return pwd_context.hash(password)
