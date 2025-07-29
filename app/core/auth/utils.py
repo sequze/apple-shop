bd8aa2d918c8f6961fa95f0016722ffe0e95e1ca
Амир Абдрафиков <sequze@mail.ru>
2025-07-29 18:39:28 +0300

    feat: hashing password and validating by bcrypt

--- app/core/auth/utils.py
+++ app/core/auth/utils.py
@@ -26,12 +26,9 @@ def decode_jwt(
     return decoded_jwt
 
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-
 def verify_password(plain_password: str, hashed_password: str) -> bool:
-    return pwd_context.verify(plain_password, hashed_password)
+    return bcrypt.checkpw(plain_password.encode("utf-8"), hashed_password.encode("utf-8"))
 
 
 def get_password_hash(password: str) -> str:
-    return pwd_context.hash(password)
+    return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt()).decode("utf-8")
