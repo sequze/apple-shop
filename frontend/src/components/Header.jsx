3bbdb7c1bb7d106affb6abe99fd551279b6e739a
Aigiz Tulybaev <flaylay256@gmail.com>
2025-08-04 23:23:17 +0500

    feat: start adaptive

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -11,6 +11,7 @@ const Header = (props) => {
     const [isAboutModalOpen, setIsAboutModalOpen] = useState(false);
     const [isContactModalOpen, setIsContactModalOpen] = useState(false);
     const [isAuth, setIsAuth] = useState(null);
+    const [isBurgerOpen, setIsBurgerOpen] = useState(false);
 
     useEffect(() => {
         const check = async () => {
@@ -24,42 +25,74 @@ const Header = (props) => {
         check();
     }, []);
 
-    const menuLink = "cursor-pointer transition delay-150 duration-200 ease-in-out hover:opacity-50";
+    useEffect(() => {
+        if (isBurgerOpen) {
+            document.body.style.overflow = "hidden";
+        } else {
+            document.body.style.overflow = "auto";
+        }
+
+        return () => {
+            document.body.style.overflow = "auto";
+        }
+    }, [isBurgerOpen]);
+
+    const menuLink = "cursor-pointer transition delay-250 duration-200 ease-in-out hover:opacity-50";
+    const burgerLine = "block w-10 h-1 bg-black rounded-full  transition-all duration-200";
 
     return (
-        <header className="fixed w-full bg-[#fff] z-[100] shadow">
+        <header className={`${isBurgerOpen ? "h-full bg-transparent" : ""}
+                fixed w-full bg-[#fff] z-[100] shadow flex flex-col items-center px-4 sm:px-10 lg:px-[60px]`}>
+            <div className={`${isBurgerOpen ? "pointer-events-auto" : "-translate-y-[80%] opacity-0 pointer-events-none"} h-[100vh] absolute w-full bg-[#fff] transition-transform duration-300`} />
             {isAuth == null ? (
                 <Loader />
             ) : (
                 <>
-                    <div className="mx-auto max-w-[1500px] w-full">
-                        <div className="flex justify-between w-full h-[75px] truncate items-center">
-                            <Link to="/">
+                    <div className={`${isBurgerOpen ? "h-full" : ""} max-w-[1500px] w-full relative`}>
+                        <div className={`
+                         ${isBurgerOpen ? "h-full justify-center" : "justify-between "} flex w-full h-[75px] truncate items-center`}>
+                            <Link className={ isBurgerOpen ? "absolute top-0 -translate-y-9 left-0" : ""} to="/" onClick={() => setIsBurgerOpen(false)}>
                                 <img className="max-w-[150px]" src={props.logo} alt="Логотип"/>
                             </Link>
                             <nav>
-                                <ul className="flex gap-[50px] list-none inter-300">
+                                <button
+                                    className={`${isBurgerOpen ? "top-0 translate-y-1/2 -translate-x-1/2" : "top-1/2 -translate-y-1/2"} absolute right-0 lg:hidden space-y-2 p-2`}
+                                    onClick={() => setIsBurgerOpen(!isBurgerOpen)}>
+                                    <span className={(isBurgerOpen ? "rotate-45 translate-y-2.5 " : " ") + burgerLine}></span>
+                                    <span className={isBurgerOpen ? "opacity-0 " : "" + burgerLine}></span>
+                                    <span className={(isBurgerOpen ? "-rotate-45  " : "") + burgerLine}></span>
+                                </button>
+                                <ul className={`${
+                                    isBurgerOpen ? "flex flex-col items-center text-[24px]" : "hidden"
+                                }
+                                    lg:flex gap-[50px] list-none inter-300`}>
                                     <li className={menuLink}>
-                                        <Link to="/">Главная</Link>
+                                        <Link to="/" onClick={() => setIsBurgerOpen(false)}>Главная</Link>
                                     </li>
-                                    <li className={menuLink} onClick={() => setIsAboutModalOpen(true)}>
+                                    <li className={menuLink} onClick={() => {
+                                        setIsAboutModalOpen(true);
+                                        setIsBurgerOpen(false);
+                                    }}>
                                         О нас
                                     </li>
-                                    <li className={menuLink} onClick={() => setIsContactModalOpen(true)}>
+                                    <li className={menuLink} onClick={() => {
+                                        setIsContactModalOpen(true);
+                                        setIsBurgerOpen(false);
+                                    }}>
                                         Контакты
                                     </li>
                                     {isAuth ? (
                                         <>
                                             <li className={menuLink}>
-                                                <Link to="/bucket">Корзина</Link>
+                                                <Link to="/bucket" onClick={() => setIsBurgerOpen(false)}>Корзина</Link>
                                             </li>
                                             <li className={menuLink}>
-                                                <Link to="/profile">Профиль</Link>
+                                                <Link to="/profile" onClick={() => setIsBurgerOpen(false)}>Профиль</Link>
                                             </li>
                                         </>
                                     ) : (
                                         <li className={menuLink}>
-                                            <Link to="/register">Зарегистрироваться</Link>
+                                            <Link to="/register" onClick={() => setIsBurgerOpen(false)}>Зарегистрироваться</Link>
                                         </li>
                                     )}
                                 </ul>
