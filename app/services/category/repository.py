75ece9ae873d62038eebbe06cf6d6a80681a6517
Амир Абдрафиков <sequze@mail.ru>
2025-08-07 16:44:24 +0300

    feat: update category repository

--- app/services/category/repository.py
+++ app/services/category/repository.py
@@ -8,11 +8,6 @@ from core.repositories.base_repository import SQlAlchemyRepository
 class CategoryRepository(SQlAlchemyRepository):
     model = Category
 
-    async def get_with_children(self, session: AsyncSession, id: int):
-        res = await session.scalar(select(Category)
-                                   .where(Category.id == id)
-                                   .options(joinedload(Category.children)))
-        return res
 
     async def get_with_products(self, session: AsyncSession, id: int) -> Category:
         res = await session.scalar(select(Category)
