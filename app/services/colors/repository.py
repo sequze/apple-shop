b98ac7b4f4a8faafd8b2c70dd044f9431b3b4552
Амир Абдрафиков <sequze@mail.ru>
2025-08-09 15:49:01 +0300

    feat: ProductColorRepository

---
+++ app/services/colors/repository.py
@@ -0,0 +1,13 @@
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from core.models import ProductColor
+from core.repositories.base_repository import SQlAlchemyRepository
+
+
+class ProductColorRepository(SQlAlchemyRepository):
+    model = ProductColor
+    async def get_by_product_id(self, session: AsyncSession, product_id: int) -> list[ProductColor]:
+        stmt = select(ProductColor).where(ProductColor.product_id == product_id)
+        result = await session.scalars(stmt)
+        return list(result)
