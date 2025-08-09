2405b802d6d22f2fdce02ccf89fe674d3d9f80e1
Амир Абдрафиков <sequze@mail.ru>
2025-08-09 15:46:48 +0300

    feat: add product color and rework relationships

---
+++ app/core/models/product_color.py
@@ -0,0 +1,24 @@
+from typing import TYPE_CHECKING
+
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from .base import Base
+from .mixins import IntIdPkMixin
+
+if TYPE_CHECKING:
+    from .product_image import ProductImage
+    from .product import Product
+
+class ProductColor(Base, IntIdPkMixin):
+    product_id: Mapped[int] = mapped_column(ForeignKey("products.id"))
+    name: Mapped[str]
+    stock: Mapped[int]
+
+    product: Mapped["Product"] = relationship(back_populates="colors")
+    images: Mapped[list["ProductImage"]] = relationship(
+        back_populates="color",
+        lazy="selectin",
+        cascade="all, delete-orphan",
+        passive_deletes=True,
+    )
