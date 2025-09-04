7fcce2678f43ffd35ee91d83062a96688c509bb0
Амир Абдрафиков <sequze@mail.ru>
2025-09-04 14:55:06 +0300

    feat: add UserRepository tests

---
+++ tests/unit_tests/test_users/test_repository.py
@@ -0,0 +1,24 @@
+from services.user.repository import UserRepository
+from core.models import db_helper
+import pytest
+
+
+@pytest.mark.parametrize(
+    "id, email, exists",
+    [
+        (1, "john.doe@example.com", True),
+        (2, "jane.smith@example.com", True),
+        (12, "", False),
+    ],
+)
+async def test_find_user_by_id(id, email, exists):
+    async with db_helper.session_factory() as session:
+        repository = UserRepository()
+        user = await repository.get_by_id(session, id)
+
+        if exists:
+            assert user
+            assert user.id == id
+            assert user.email == email
+        else:
+            assert not user
