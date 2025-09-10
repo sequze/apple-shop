d1f970b71023284cb6226785bbe0ed8c5519a3d8
Амир Абдрафиков <sequze@mail.ru>
2025-09-10 14:36:43 +0300

    feat: add user and admin fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -19,8 +19,8 @@ def open_mock_json(model: str):
 cart_items = open_mock_json("cart_items")
 categories = open_mock_json("categories")
 discounts = open_mock_json("discounts")
-order_items = open_mock_json("order_items")
-orders = open_mock_json("orders")
+# order_items = open_mock_json("order_items")
+# orders = open_mock_json("orders")
 product_characteristics = open_mock_json("product_characteristics")
 product_colors = open_mock_json("product_colors")
 product_images = open_mock_json("product_images")
@@ -32,8 +32,8 @@ for user in users:
     user["created_at"] = datetime.fromisoformat(user["created_at"])
 for product in products:
     product["created_at"] = datetime.fromisoformat(product["created_at"])
-for order in orders:
-    order["created_at"] = datetime.fromisoformat(order["created_at"])
+# for order in orders:
+#     order["created_at"] = datetime.fromisoformat(order["created_at"])
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -50,16 +50,16 @@ async def prepare_database():
         add_products = insert(Product).values(products)
         add_users = insert(User).values(users)
         add_colors = insert(ProductColor).values(product_colors)
-        add_orders = insert(Order).values(orders)
-        add_order_items = insert(OrderItem).values(order_items)
+        # add_orders = insert(Order).values(orders)
+        # add_order_items = insert(OrderItem).values(order_items)
 
         await session.execute(add_categories)
         await session.execute(add_products)
         await session.execute(add_users)
         await session.execute(add_colors)
         await session.execute(add_cart_items)
-        await session.execute(add_orders)
-        await session.execute(add_order_items)
+        # await session.execute(add_orders)
+        # await session.execute(add_order_items)
 
         await session.commit()
 
@@ -80,8 +80,21 @@ async def ac():
         yield ac
 
 
-@pytest.fixture(scope="function")
-async def authenticated_ac():
+@pytest.fixture(scope="session")
+async def user_ac():
+    async with AsyncClient(
+        transport=ASGITransport(app=fastapi_app), base_url="http://test"
+    ) as ac:
+        response = await ac.post(
+            "/api/auth/login", json={"email": "user@example.com", "password": "user"}
+        )
+        assert response.status_code == 200
+        ac.headers["Authorization"] = f"Bearer {response.json()['access_token']}"
+        yield ac
+
+
+@pytest.fixture(scope="session")
+async def admin_ac():
     async with AsyncClient(
         transport=ASGITransport(app=fastapi_app), base_url="http://test"
     ) as ac:
