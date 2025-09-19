59c2114e15139170a65e29b48080d4fca28619c0
Амир Абдрафиков <sequze@mail.ru>
2025-09-19 23:25:56 +0300

    feat: add grafana logging

--- app/main.py
+++ app/main.py
@@ -7,18 +7,20 @@ from api import router as api_router
 from core.config import settings
 from core.models.db_helper import db_helper
 
+from prometheus_config import PrometheusMiddleware, metrics
+
 
 @asynccontextmanager
 async def lifespan(fastapi_app: FastAPI):
     yield
     await db_helper.dispose()
+
+
 app = FastAPI(
     lifespan=lifespan,
 )
 
 origins = [
-    "http://localhost.tiangolo.com",
-    "https://localhost.tiangolo.com",
     "http://localhost",
     "http://localhost:8080",
     "http://localhost:5173",
@@ -32,10 +34,14 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
+app.add_middleware(PrometheusMiddleware, app_name="fastapi")
+app.add_route("/metrics", metrics)
+
 app.include_router(
     api_router,
     prefix=settings.api_prefix.prefix,
 )
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", reload=True)
+    uvicorn.run("main:app", reload=True, host="0.0.0.0", port=8000)
